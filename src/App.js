ce7955d7443977333297bd7ac1c79c3b72047f2e
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-05-05 16:11:52 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+
+//this is a component
+function App() {
+
+  return (
+    <>
+      <div className="App">
+          <h1>Hello, here are the top movies</h1>
+      </div>
+      <div>
+        <h3>Top movies</h3>
+        <ol>
+          <li>Catch me if you can</li>
+          <li>Shawshank Redemption</li>
+          <li>Don't be a menace</li>
+          <li>Harold and Kumar go to white castle</li>
+
+        </ol>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
